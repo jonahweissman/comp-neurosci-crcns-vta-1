44d49d7d1f55ad398726736556ee845ab756b799
Ekaterina Stepanova <stepanovacz.gmail.com>
2021-04-16 19:01:29 -0400

    clean up io

--- src/io.py
+++ src/io.py
@@ -8,35 +8,33 @@ from os import path
 import parse
 import pandas as pd
 
-def load_spikes():
+def _load_data(column):
     spikes = []
     for file in glob.glob(path.join("data","vta1","data","*")):
         fileinfo = parse.parse(path.join("data","vta1", "data","{treatment}_{task}_{trial}.mat"), file).named
         mat = loadmat(file)
-        for spike in mat["spiketimes"]:
+        for spike in mat[column]:
             d = {
                  "time": spike[0] 
                  }
             d.update(fileinfo)
             spikes.append(d) 
-
+    if len(spikes) == 0
+        raise ValueError("Missing data. See data/README.md file on instructions for downloading the data")
+        
     spiket = pd.DataFrame(spikes)        
     spiket = spiket.set_index(["treatment", "task", "trial"])
     return spiket
+
+def load_spikes():
+    return _load_data("spiketimes"):
+    
+
         
-def load_odor():        
-    odor_onsets = []
-    for file in glob.glob(path.join("data","vta1","data","*")):
-        fileinfo = parse.parse(path.join("data","vta1", "data","{treatment}_{task}_{trial}.mat"), file).named
-        mat = loadmat(file)
-        for odor in mat["odor_onsets"]:
-            d = {
-                 "time": odor[0] 
-                 }
-            d.update(fileinfo)
-            odor_onsets.append(d)
+def load_odor():
+    return _load_data("odor_onsets")
+    
 
 
-    odor_onsets = pd.DataFrame(odor_onsets)
-    odor_onsets = odor_onsets.set_index(["treatment", "task", "trial"])
-    return odor_onsets
+    
+    
