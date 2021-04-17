9cae319e4a2a1a1fc1017c479cf723ac6a7f8bea
Jonah Weissman <jonahrweissman@gmail.com>
2021-04-16 23:21:39 -0400

    Update setup.py with package name

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ from setuptools import setup
 if sys.hexversion < 0x02070000:
     raise RuntimeError("Python 2.7 or higher required")
 
-# Replace all instances of `comp-neurosci-skeleton` with the name of your project
 setup(
-    name="comp-neurosci-skeleton",
+    name="comp-neurosci-crcns-vta-1",
     version="0.0.1",
-    package_dir={'comp-neurosci-skeleton': 'src'},
-    packages=["comp-neurosci-skeleton"],
+    package_dir={'comp-neurosci-crcns-vta-1': 'src'},
+    packages=["comp-neurosci-crcns-vta-1"],
 
     description="",
     long_description="",
     install_requires=[
         "numpy>=1.10",
+        "parse>=1.19",
+        "pandas>=1.2",
     ],
 
-    author="Your Name Here",
-    maintainer='Your Name Here',
+    author="Katherine Nimchuk, Ekaterina Stepanova, Emma Whelan, Jonah Weissman",
 )
