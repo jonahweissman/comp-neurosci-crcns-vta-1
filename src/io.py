6ef9834bf727e0fe1f1143a0e100fad20d1ae830
Jonah Weissman <jonahrweissman@gmail.com>
2021-04-26 21:04:33 -0400

    Add missing data columns in I/O functions
    Our original code did not include the columns
    "reward_onsets" or "trial_type". I rewrote the entire IO
    module to make this easier and added better documentation
    for the data.

--- src/io.py
+++ src/io.py
@@ -3,40 +3,56 @@
 """Functions for file IO"""
 from __future__ import print_function, division, absolute_import
 import glob
-from scipy.io import loadmat 
+from scipy.io import loadmat
 from os import path
 import parse
 import pandas as pd
 
-def _load_data(column):
-    spikes = []
+def _load_data(columns):
+    times = []
     for file in glob.glob(path.join("data","vta1","data","*")):
-        fileinfo = parse.parse(path.join("data","vta1", "data","{treatment}_{task}_{trial}.mat"), file).named
+        fileinfo = parse.parse(
+            path.join("data","vta1", "data","{treatment}_{task}_{trial}.mat"),
+            file,
+        ).named
         mat = loadmat(file)
-        for spike in mat[column]:
-            d = {
-                 "time": spike[0] 
-                 }
-            d.update(fileinfo)
-            spikes.append(d) 
-    if len(spikes) == 0:
-        raise ValueError("Missing data. See data/README.md file on instructions for downloading the data")
+        event_categories = {c: mat[c][:, 0] for c in columns}
+        df = pd.DataFrame(event_categories)
+        for key, value in fileinfo.items():
+            df[key] = value
+        df = df.set_index(["treatment", "task", "trial"])
         
-    spiket = pd.DataFrame(spikes)        
-    spiket = spiket.set_index(["treatment", "task", "trial"])
-    return spiket
+        times.append(df)
+
+    if len(times) == 0:
+        raise ValueError("Data not found. See data/README.md file"
+                         " on instructions for downloading the data")
+    # combine DataFrame for each file into a single DataFrame
+    times_df = times[0].append(times[1:] if len(times) > 1 else [])
+    return times_df
 
 def load_spikes():
-    """ return spike_train derived from crcns vta-1 dataset"""
-    return _load_data("spiketimes")
+    """ returns spike times from vta-1 dataset
     
-
-        
-def load_odor():
-    """ return odor onsets derived from crcns vta-1 dataset"""
-    return _load_data("odor_onsets")
-    
+    # DataFrame columns:
+    - spiketimes: time of spiking of optogenetically-identified dopamine
+                  neurons in the mouse ventral tegmental area in milliseconds
+    """
+    return _load_data(["spiketimes"])
 
 
+def load_trial_data():
+    """ returns odor and reward onsets from vta-1 dataset
     
-    
+    # DataFrame columns:
+    - odor_onsets: time of odor onset in milliseconds
+    - reward_onsets: time of reward onset in milliseconds
+    - trial_type:
+        - 1-9 correspond to Odor A Trials, with ISIs of 1.2s (1) to 2.8s (9).
+        - 10 and 11 correspond to Odor B and Odor C trials, respectively
+        - 16 corresponds to Odor D trials
+        - 13 corresponds to Odor A omission trials
+        - 14 corresponds to Odor B omission trials
+        - 15 corresponds to Odor C omission trials
+    """
+    return _load_data(["odor_onsets", "reward_onsets", "trial_type"])
